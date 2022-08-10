d4525ca35f1815d049e5f76a40980c1b075a5b4d
zackbog <101665281+zackbog@users.noreply.github.com>
2022-08-10 19:18:52 -0400

    Add files via upload

---
+++ Project2.c
@@ -0,0 +1,20 @@
+/*
+Author: Zack Bogden, James Lewandowski
+Assignment Number: 2
+Date of Submission: 
+Name of this file: Project2.c
+Short description of contents:
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(){
+
+    printf("Hello");
+    return 0;
+}
